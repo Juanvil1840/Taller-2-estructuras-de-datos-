8a8dabb98b56b01c038c939a46e784f52fc4a6fc
Juan Mendoza <mendozajuanjose348@gmail.com>
2025-08-17 23:50:37 -0500

    Update Polinomio.hxx

--- Polinomio.hxx
+++ Polinomio.hxx
@@ -30,8 +30,35 @@ template < class S >
 Polinomio<S> Polinomio<S>::operator+( const Polinomio< S >& der ) const //Agregado por Lina Perez
 {
   Polinomio<S> resultado;
-
+  //Agregado por Juan Mendoza
   // TODO #1
+  // Encuentra el mayor grado entre los dos polinomios 
+  unsigned int maxGradoIzq = this->size();
+  unsigned int maxGradoDer = der.size();
+  unsigned int maxGrado = std::max(maxGradoIzq, maxGradoDer);
+
+  // Ajusta el tamaño del polinomio resultado con ceros
+  resultado.resize(maxGrado, S(0));
+
+  // Suma coeficientes grado a grado
+  for (unsigned int i = 0; i < maxGrado; ++i) {
+    S coefIzq, coefDer;
+
+    // Obtener coeficiente del polinomio izquierdo
+    if (i < maxGradoIzq)
+    coefIzq = (*this)[i];
+    else
+    coefIzq = S(0);
+
+    // Obtener coeficiente del polinomio derecho
+    if (i < maxGradoDer)
+    coefDer = der[i];
+    else
+    coefDer = S(0);
+
+    // Guardar la suma en el resultado
+    resultado[i] = coefIzq + coefDer;
+    }
 
   return resultado;
 }
